e1d4c50d20d5bdccdaf7bfeeed76df5c23ac4a6d
Enrique-FB-GFT <eefc@gft.com>
2025-05-13 16:33:01 +0200

    Made regex a final constant

--- src/main/java/com/gft/user/domain/model/user/Email.java
+++ src/main/java/com/gft/user/domain/model/user/Email.java
@@ -5,6 +5,8 @@ import org.jmolecules.ddd.annotation.ValueObject;
 @ValueObject
 public record Email(String value) {
 
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
     public Email {
         if (!isValid(value)) {
             throw new IllegalArgumentException("Invalid value");
@@ -12,7 +14,7 @@ public record Email(String value) {
     }
 
     private boolean isValid(String email) {
-        return email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+        return email.matches(EMAIL_REGEX);
     }
 
 }
