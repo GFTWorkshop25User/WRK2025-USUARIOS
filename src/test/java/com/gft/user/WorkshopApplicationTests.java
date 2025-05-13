8e35ab251d00e4bc3c245ec9d95ccadada5b7f95
jpgo <jpgo@gft.com>
2025-05-13 13:31:00 +0200

    100 coverage

--- src/test/java/com/gft/user/WorkshopApplicationTests.java
+++ src/test/java/com/gft/user/WorkshopApplicationTests.java
@@ -3,11 +3,19 @@ package com.gft.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class WorkshopApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testMain() {
+		WorkshopApplication.main(new String[] {});
+		assertTrue(true);
 	}
 
 }
