f7116526252c56034a1b2cc79ee852e0686150c3
jpgo <jpgo@gft.com>
2025-05-13 13:24:48 +0200

    added @Generated annotation

--- src/main/java/com/gft/user/WorkshopApplication.java
+++ src/main/java/com/gft/user/WorkshopApplication.java
@@ -1,9 +1,11 @@
 package com.gft.user;
 
+import jakarta.annotation.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Generated("Spring Boot Main Class")
 public class WorkshopApplication {
 
 	public static void main(String[] args) {
