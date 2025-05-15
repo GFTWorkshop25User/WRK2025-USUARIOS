387ce346dcb3d833eb7c0d1e509ebcc64cc39e81
jpgo <jpgo@gft.com>
2025-05-15 09:21:05 +0200

    created ddd and architecture tests

--- src/test/java/com/gft/user/JMoleculesDddUnitTest.java
+++ src/test/java/com/gft/user/JMoleculesDddUnitTest.java
@@ -1,15 +1,33 @@
 package com.gft.user;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.junit.ArchTest;
-import org.jmolecules.archunit.JMoleculesDddRules;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.jmolecules.ddd.annotation.AggregateRoot;
+import org.jmolecules.ddd.annotation.ValueObject;
+import org.junit.jupiter.api.Test;
 
 @AnalyzeClasses(packages = "com.gft.user")
 class JMoleculesDddUnitTest {
 
-    @ArchTest
-    void whenCheckingAllClasses_thenCodeFollowsAllDddPrinciples(JavaClasses classes) {
-        JMoleculesDddRules.all().check(classes);
+    @Test
+    void aggregateRootsShouldResideInDomainPackage() {
+        JavaClasses classes = new ClassFileImporter().importPackages("com.gft.user");
+
+        ArchRuleDefinition.classes()
+                .that().areAnnotatedWith(AggregateRoot.class)
+                .should().resideInAPackage("..domain..")
+                .check(classes);
+    }
+
+    @Test
+    void valueObjectsShouldResideInDomainPackage() {
+        JavaClasses classes = new ClassFileImporter().importPackages("com.gft.user");
+
+        ArchRuleDefinition.classes()
+                .that().areAnnotatedWith(ValueObject.class)
+                .should().resideInAPackage("..domain..")
+                .check(classes);
     }
 }
