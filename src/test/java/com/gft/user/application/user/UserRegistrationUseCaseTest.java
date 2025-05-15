45bbd68901d04453ff6bbd6d53d7b2ac05ddaa11
jpgo <jpgo@gft.com>
2025-05-15 16:02:19 +0200

    fixed UserRegistrationUseCaseTest

--- src/test/java/com/gft/user/application/user/UserRegistrationUseCaseTest.java
+++ src/test/java/com/gft/user/application/user/UserRegistrationUseCaseTest.java
@@ -1,26 +1,38 @@
 package com.gft.user.application.user;
 
 import com.gft.user.application.user.dto.UserRequest;
+import com.gft.user.domain.model.user.User;
+import com.gft.user.domain.repository.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.UUID;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class UserRegistrationUseCaseTest {
 
-    @Autowired
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
     private UserRegistrationUseCase userRegistrationUseCase;
 
     @Test
     void should_register_when_userRequestIsValid() {
+        UserRequest userRequest = new UserRequest("Manolo", "manolo@mail.com", "Manolito12345!");
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
 
-        UserRequest userRequest = new UserRequest("username", "username@mail.com", "Password123456!");
-        UUID uuid = userRegistrationUseCase.execute(userRequest);
+        userRegistrationUseCase.execute(userRequest);
+        verify(userRepository, times(1)).save(userCaptor.capture());
 
-        assertNotNull(uuid);
+        User capturedUser = userCaptor.getValue();
+        assertEquals("Manolo", capturedUser.getName());
+        assertEquals("manolo@mail.com", capturedUser.getEmail().value());
     }
 }
