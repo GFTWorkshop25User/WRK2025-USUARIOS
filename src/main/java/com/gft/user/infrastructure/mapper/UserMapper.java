3af50b8a82657a4f6d14a5c4ad80c00b3d0ddab7
Enrique-FB-GFT <eefc@gft.com>
2025-05-15 15:47:05 +0200

    Implemented methods user mapped

--- src/main/java/com/gft/user/infrastructure/mapper/UserMapper.java
+++ src/main/java/com/gft/user/infrastructure/mapper/UserMapper.java
@@ -1,7 +1,6 @@
 package com.gft.user.infrastructure.mapper;
 
-import com.gft.user.domain.model.user.FavoriteId;
-import com.gft.user.domain.model.user.User;
+import com.gft.user.domain.model.user.*;
 import com.gft.user.infrastructure.entity.UserEntity;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +16,29 @@ public class UserMapper {
     }
 
     public UserEntity toUserEntity(User user) {
-
-        return new UserEntity(user.getId().getUuid(), user.getName(), user.getEmail().value(), user.getPassword().getHashedValue(), addressMapper.toAddressEntity(user.getAddress()), user.getFavoriteProductIds().stream().map(FavoriteId::value).collect(Collectors.toSet()), user.getLoyaltyPoints().getValue(), user.isDisabled());
+        return new UserEntity(
+                user.getId().getUuid(),
+                user.getName(),
+                user.getEmail().value(),
+                user.getPassword().getHashedValue(),
+                addressMapper.toAddressEntity(user.getAddress()),
+                user.getFavoriteProductIds().stream().map(FavoriteId::value).collect(Collectors.toSet()),
+                user.getLoyaltyPoints().getValue(),
+                user.isDisabled()
+        );
     }
+
+    public User fromUserEntity(UserEntity userEntity) {
+        return User.create(
+                UserId.create(userEntity.getId()),
+                userEntity.getName(),
+                new Email(userEntity.getEmail()),
+                new Password(new SafeString(userEntity.getHashedPassword())),
+                addressMapper.fromAddressEntity(userEntity.getAddress()),
+                userEntity.getFavoriteProductIds().stream().map(FavoriteId::new).collect(Collectors.toSet()),
+                new LoyaltyPoints(userEntity.getLoyaltyPoints()),
+                userEntity.isDisabled()
+        );
+    }
+
 }
