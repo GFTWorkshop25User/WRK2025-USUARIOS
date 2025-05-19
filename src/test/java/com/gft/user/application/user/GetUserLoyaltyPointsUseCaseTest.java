f3ff0c714c29902c220eec6ff54753e8372bf702
jpgo <jpgo@gft.com>
2025-05-19 12:44:36 +0200

    fixed architecture getting loyalty points

--- src/test/java/com/gft/user/application/user/GetUserLoyaltyPointsUseCaseTest.java
+++ src/test/java/com/gft/user/application/user/GetUserLoyaltyPointsUseCaseTest.java
@@ -1,5 +1,6 @@
 package com.gft.user.application.user;
 
+import com.gft.user.domain.model.user.*;
 import com.gft.user.domain.repository.UserRepository;
 import com.gft.user.infrastructure.exception.UserNotFoundException;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashSet;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,14 +29,23 @@ class GetUserLoyaltyPointsUseCaseTest {
     @Test
     void should_returnPoints_when_execute() {
         UUID uuid = UUID.randomUUID();
+        User user = User.create(
+                new UserId(),
+                "miguel",
+                new Email("miguel@gmail.com"),
+                Password.createPasswordFromHashed("$2a$10$hZwpOSjHC/eNQAqFYDHG4OuVDQ1U.JX6QKg/fBi9uML.Xp/p8h8qe!!"),
+                new Address("Spain", "241852", "Villalba", "Calle los floriponcios"),
+                new HashSet<>(),
+                new LoyaltyPoints(20),
+                false
+        );
 
         when(userRepository.existsById(uuid)).thenReturn(true);
-
-        when(userRepository.getLoyaltyPoints(uuid)).thenReturn(1);
+        when(userRepository.getById(uuid)).thenReturn(user);
 
         int points = getUserLoyaltyPointsUseCase.execute(uuid);
 
-        assertEquals(1, points);
+        assertEquals(20, points);
 
     }
 
