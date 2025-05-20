7690b199e559a612b70726b0a0e60c809fb1bf53
jpgo <jpgo@gft.com>
2025-05-20 15:00:09 +0200

    created Notification dtos and mapper

---
+++ src/main/java/com/gft/user/infrastructure/mapper/NotificationMapper.java
@@ -0,0 +1,17 @@
+package com.gft.user.infrastructure.mapper;
+
+import com.gft.user.application.notification.dto.NotificationDto;
+import com.gft.user.infrastructure.dto.NotificationResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationMapper {
+
+    public NotificationDto toNotificationDto(NotificationResponse notificationResponse) {
+        return new NotificationDto(
+                notificationResponse.message(),
+                notificationResponse.createdAt(),
+                notificationResponse.important()
+        );
+    }
+}
---
+++ src/test/java/com/gft/user/infrastructure/mapper/NotificationMapperTest.java
@@ -0,0 +1,36 @@
+package com.gft.user.infrastructure.mapper;
+
+import com.gft.user.application.notification.dto.NotificationDto;
+import com.gft.user.infrastructure.dto.NotificationResponse;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NotificationMapperTest {
+
+    NotificationMapper notificationMapper = new NotificationMapper();
+
+    @Test
+    void should_mapNotification() {
+        LocalDateTime now = LocalDateTime.now();
+
+        NotificationResponse notificationResponse = new NotificationResponse(
+                UUID.randomUUID(),
+                now,
+                UUID.randomUUID(),
+                "Message",
+                true
+        );
+
+        NotificationDto notificationDto = new NotificationDto(
+                "Message",
+                now,
+                true
+        );
+
+        assertEquals(notificationDto, notificationMapper.toNotificationDto(notificationResponse));
+    }
+}
