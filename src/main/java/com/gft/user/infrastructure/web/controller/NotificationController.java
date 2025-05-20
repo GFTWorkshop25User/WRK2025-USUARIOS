38d9b52e0c279d5dc397ef1d0b1c74c9d0eb2a62
Enrique-FB-GFT <eefc@gft.com>
2025-05-20 16:06:01 +0200

    WRK25USERS-56 Notifications controller and tests

---
+++ src/main/java/com/gft/user/infrastructure/web/controller/NotificationController.java
@@ -0,0 +1,26 @@
+package com.gft.user.infrastructure.web.controller;
+
+
+import com.gft.user.application.notification.usecase.GetUserNotificationsUseCase;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/notifications")
+@AllArgsConstructor
+public class NotificationController {
+
+    GetUserNotificationsUseCase getUserNotificationsUseCase;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserNotifications(@PathVariable UUID id) {
+        return ResponseEntity.ok(getUserNotificationsUseCase.execute(id));
+    }
+
+}
