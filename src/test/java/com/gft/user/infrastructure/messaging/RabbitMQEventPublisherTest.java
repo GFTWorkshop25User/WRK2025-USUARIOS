d7f296d9414a9f628c0a15e0e82a91eab9e2de11
jpgo <jpgo@gft.com>
2025-05-21 12:54:52 +0200

    created user disable event and producer

---
+++ src/test/java/com/gft/user/infrastructure/messaging/RabbitMQEventPublisherTest.java
@@ -0,0 +1,33 @@
+package com.gft.user.infrastructure.messaging;
+
+import com.gft.user.domain.event.UserDisabledEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class RabbitMQEventPublisherTest {
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    @InjectMocks
+    private RabbitMQEventPublisher eventPublisher;
+
+    @Test
+    void should_publishEvent_when_sendUserDisabledMessage() {
+        UserDisabledEvent userDisabledEvent = new UserDisabledEvent(UUID.randomUUID());
+
+        eventPublisher.sendUserDisabledMessage(userDisabledEvent);
+
+        verify(rabbitTemplate, times(1)).convertAndSend("user", "disabled", userDisabledEvent);
+    }
+}
