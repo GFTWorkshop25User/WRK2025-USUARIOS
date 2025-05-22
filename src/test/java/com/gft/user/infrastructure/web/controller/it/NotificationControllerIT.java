4b538cc52e3cf7efa82c526e42d0aacbd64d95a1
jpgo <jpgo@gft.com>
2025-05-22 11:59:00 +0200

    created spring profiles

--- src/test/java/com/gft/user/infrastructure/web/controller/it/NotificationControllerIT.java
+++ src/test/java/com/gft/user/infrastructure/web/controller/it/NotificationControllerIT.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -27,6 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@ActiveProfiles("test")
 @WebMvcTest(NotificationController.class)
 class NotificationControllerIT {
 
