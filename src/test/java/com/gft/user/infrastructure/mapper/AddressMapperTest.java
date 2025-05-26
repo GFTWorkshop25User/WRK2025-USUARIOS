7d066fad3935ad4daca340cc277b01c8d3877243
jpgo <jpgo@gft.com>
2025-05-26 08:45:04 +0200

    WRK25USERS-80 removed unused imports

--- src/test/java/com/gft/user/infrastructure/mapper/AddressMapperTest.java
+++ src/test/java/com/gft/user/infrastructure/mapper/AddressMapperTest.java
@@ -3,12 +3,8 @@ package com.gft.user.infrastructure.mapper;
 import com.gft.user.domain.model.user.Address;
 import com.gft.user.infrastructure.entity.AddressEntity;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class AddressMapperTest {
