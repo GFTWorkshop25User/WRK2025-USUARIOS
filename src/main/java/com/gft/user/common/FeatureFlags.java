209ff6d2a7c39028b5bd016363716c604db2d12e
Enrique-FB-GFT <eefc@gft.com>
2025-05-26 16:28:21 +0200

    WRK25USERS-82 Bypass Test Getter/Setter

--- src/main/java/com/gft/user/common/FeatureFlags.java
+++ src/main/java/com/gft/user/common/FeatureFlags.java
@@ -1,5 +1,6 @@
 package com.gft.user.common;
 
+import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 @Setter
+@Generated
 public class FeatureFlags {
     @Value("${app.toggleNotifications}")
     private volatile boolean toggleNotifications;
