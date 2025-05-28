7e41d114a79531f6b44f5a490aaf7d0f47665730
jpgo <jpgo@gft.com>
2025-05-28 08:51:04 +0200

    removed unused imports

--- src/test/java/com/gft/user/e2etest/CommunicationE2EIT.java
+++ src/test/java/com/gft/user/e2etest/CommunicationE2EIT.java
@@ -1,7 +1,5 @@
 package com.gft.user.e2etest;
 
-import com.gft.user.application.dto.NotificationDto;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
