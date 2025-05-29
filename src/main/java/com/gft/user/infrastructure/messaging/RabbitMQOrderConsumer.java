a917d833ce4df527c34d2cefaf8f5689049f77a0
jpgo <jpgo@gft.com>
2025-05-29 16:21:57 +0200

    fixed logger initialization

--- src/main/java/com/gft/user/infrastructure/messaging/RabbitMQOrderConsumer.java
+++ src/main/java/com/gft/user/infrastructure/messaging/RabbitMQOrderConsumer.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitMQOrderConsumer {
 
-    Logger logger = LoggerFactory.getLogger(RabbitMQNotificationConsumer.class);
+    Logger logger = LoggerFactory.getLogger(RabbitMQOrderConsumer.class);
     private final IncrementLoyaltyPointsUseCase incrementLoyaltyPointsUseCase;
 
     public RabbitMQOrderConsumer(IncrementLoyaltyPointsUseCase incrementLoyaltyPointsUseCase) {
