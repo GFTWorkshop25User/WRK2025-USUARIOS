67447252fab377107d0e7e7c1c771128d147916f
jpgo <jpgo@gft.com>
2025-05-29 09:05:58 +0200

    added e2e tests

--- src/test/java/com/gft/user/e2etest/CommunicationE2EIT.java
+++ src/test/java/com/gft/user/e2etest/CommunicationE2EIT.java
@@ -3,6 +3,7 @@ package com.gft.user.e2etest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gft.user.application.dto.NotificationDto;
+import com.gft.user.infrastructure.dto.NotificationImportanceRequest;
 import com.gft.user.infrastructure.dto.NotificationResponse;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,8 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
@@ -44,14 +44,14 @@ class CommunicationE2EIT {
 
     @BeforeEach
     void setUp() {
-        baseUrl = "http://localhost:" + port + "/api/v1/users";
+        baseUrl = "http://localhost:" + port + "/api/v1";
         WireMock.reset();
     }
 
     @Test
     void should_getNotifications_when_userExists() throws JsonProcessingException {
         UUID userId = UUID.fromString("7d108b78-2d55-48cf-a39a-8b25bc667fe4");
-        String userNotificationsUrl = baseUrl + "/" + userId + "/notifications";
+        String userNotificationsUrl = baseUrl + "/users/" + userId + "/notifications";
 
         LocalDateTime now = LocalDateTime.now();
         List<NotificationResponse> notifications = List.of(new NotificationResponse(
@@ -78,5 +78,97 @@ class CommunicationE2EIT {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(objectMapper.writeValueAsString(notificationDtos));
+    }
+
+    @Test
+    void should_deleteNotification_when_notificationExists() {
+        UUID notificationId = UUID.randomUUID();
+        String updateNotificationImportanceUrl = baseUrl + "/notifications/" + notificationId;
+
+        stubFor(delete(urlEqualTo("/notifications/" + notificationId))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.NO_CONTENT.value())));
+
+        ResponseEntity<Void> response = restTemplate.exchange(
+                updateNotificationImportanceUrl,
+                HttpMethod.DELETE,
+                new HttpEntity<>(null),
+                Void.class
+        );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    void should_responseNotFound_when_deleteNotificationDoesntExist() {
+        UUID notificationId = UUID.randomUUID();
+        String updateNotificationImportanceUrl = baseUrl + "/notifications/" + notificationId;
+
+        stubFor(delete(urlEqualTo("/notifications/" + notificationId))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.NOT_FOUND.value())));
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                updateNotificationImportanceUrl,
+                HttpMethod.DELETE,
+                new HttpEntity<>(null),
+                String.class
+        );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isEqualTo("Notification with id " + notificationId + " not found");
+    }
+
+    @Test
+    void should_changeNotificationImportance_when_notificationExists() throws JsonProcessingException {
+        UUID notificationId = UUID.randomUUID();
+        String updateNotificationImportanceUrl = baseUrl + "/notifications/" + notificationId;
+
+        NotificationImportanceRequest notificationImportanceRequest = new NotificationImportanceRequest(true);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Boolean> requestEntity = new HttpEntity<>(true, headers);
+
+        stubFor(patch(urlEqualTo("/notifications/" + notificationId))
+                .withRequestBody(equalToJson(objectMapper.writeValueAsString(notificationImportanceRequest)))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.NO_CONTENT.value())));
+
+        ResponseEntity<Void> response = restTemplate.exchange(
+                updateNotificationImportanceUrl,
+                HttpMethod.PATCH,
+                requestEntity,
+                Void.class
+        );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    void should_responseNotFound_when_patchNotificationDoesntExist() throws JsonProcessingException {
+        UUID notificationId = UUID.randomUUID();
+        String updateNotificationImportanceUrl = baseUrl + "/notifications/" + notificationId;
+
+        NotificationImportanceRequest notificationImportanceRequest = new NotificationImportanceRequest(true);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Boolean> requestEntity = new HttpEntity<>(true, headers);
+
+        stubFor(patch(urlEqualTo("/notifications/" + notificationId))
+                .withRequestBody(equalToJson(objectMapper.writeValueAsString(notificationImportanceRequest)))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.NOT_FOUND.value())));
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                updateNotificationImportanceUrl,
+                HttpMethod.PATCH,
+                requestEntity,
+                String.class
+        );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isEqualTo("Notification with id " + notificationId + " not found");
     }
 }
