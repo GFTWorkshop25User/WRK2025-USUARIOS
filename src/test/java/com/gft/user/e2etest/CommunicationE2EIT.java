18078f3b8caf5918b8a571f59196b653c1415723
jpgo <jpgo@gft.com>
2025-05-29 07:55:50 +0200

    added wiremock to e2e test with comms

--- src/test/java/com/gft/user/e2etest/CommunicationE2EIT.java
+++ src/test/java/com/gft/user/e2etest/CommunicationE2EIT.java
@@ -1,5 +1,10 @@
 package com.gft.user.e2etest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gft.user.application.dto.NotificationDto;
+import com.gft.user.infrastructure.dto.NotificationResponse;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +14,21 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.wiremock.spring.ConfigureWireMock;
+import org.wiremock.spring.EnableWireMock;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableWireMock({
+        @ConfigureWireMock(name = "communications-client", baseUrlProperties = "app.communicationsBackendUrl")
+})
 class CommunicationE2EIT {
 
     @LocalServerPort
@@ -26,22 +37,46 @@ class CommunicationE2EIT {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private String baseUrl;
 
     @BeforeEach
     void setUp() {
         baseUrl = "http://localhost:" + port + "/api/v1/users";
+        WireMock.reset();
     }
 
     @Test
-    void should_getNotifications_when_userExists() {
+    void should_getNotifications_when_userExists() throws JsonProcessingException {
         UUID userId = UUID.fromString("7d108b78-2d55-48cf-a39a-8b25bc667fe4");
         String userNotificationsUrl = baseUrl + "/" + userId + "/notifications";
+
+        LocalDateTime now = LocalDateTime.now();
+        List<NotificationResponse> notifications = List.of(new NotificationResponse(
+                UUID.randomUUID(),
+                now,
+                userId,
+                "Message",
+                true
+        ));
+
+        List<NotificationDto> notificationDtos = List.of(new NotificationDto(
+                "Message",
+                    now,
+                    true
+        ));
+
+        stubFor(get(urlEqualTo("/notifications/" + userId))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(objectMapper.writeValueAsString(notifications))));
 
         ResponseEntity<String> response = restTemplate.getForEntity(userNotificationsUrl, String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertNotNull(response.getBody());
-        assertFalse(response.getBody().isEmpty());
+        assertThat(response.getBody()).isEqualTo(objectMapper.writeValueAsString(notificationDtos));
     }
 }
